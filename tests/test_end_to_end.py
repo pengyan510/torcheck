e213e8efa12c6d8a9b300fb484015ce6eacd873b
pengyan510 <pengyan510@gmail.com>
2021-05-26 03:39:29 +0000

    renaming to torcheck

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -2,7 +2,7 @@ import pytest
 import torch
 import torch.nn.functional as F
 from torch.optim import Adam
-import torchtest
+import torcheck
 
 
 def run_training(model, dataloader, optimizer):
@@ -16,15 +16,15 @@ def run_training(model, dataloader, optimizer):
 
 def test_changing_module_with_correct_model(correct_model, dataloader):
     optimizer = Adam(correct_model.parameters(), lr=0.001)
-    torchtest.register(optimizer)
-    torchtest.add_changing_module(correct_model, module_name="NeuralNet")
+    torcheck.register(optimizer)
+    torcheck.add_changing_module(correct_model, module_name="NeuralNet")
     run_training(correct_model, dataloader, optimizer)
 
 
 def test_changing_module_with_notchanging_model(notchanging_model, dataloader):
     optimizer = Adam(notchanging_model.parameters(), lr=0.001)
-    torchtest.register(optimizer)
-    torchtest.add_changing_module(notchanging_model, module_name="NeuralNet")
+    torcheck.register(optimizer)
+    torcheck.add_changing_module(notchanging_model, module_name="NeuralNet")
     with pytest.raises(
         RuntimeError,
         match=r"Module NeuralNet's fc1\.weight should change\.\n.*fc1.bias should change"

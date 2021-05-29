e1973f423c55b2b80fdaf04a816719b700842c50
pengyan510 <pengyan510@gmail.com>
2021-05-29 06:04:17 +0000

    adding tests

--- tests/test_param_check.py
+++ tests/test_param_check.py
@@ -53,3 +53,111 @@ def test_module_unchanging_check_with_unchanging_model(
     torcheck.add_module_unchanging_check(unchanging_model.fc1, module_name="First Layer")
     run_training(unchanging_model, dataloader, unchanging_model_optimizer)
 
+
+def test_tensor_changing_check_with_changing_model(
+    changing_model_optimizer,
+    changing_model,
+    dataloader,
+    run_training
+):
+    torcheck.register(changing_model_optimizer)
+    torcheck.add_tensor_changing_check(
+        changing_model.fc1.weight,
+        tensor_name="fc1.weight",
+        module_name="NeuralNet"
+    )
+    torcheck.add_tensor_changing_check(
+        changing_model.fc1.bias,
+        tensor_name="fc1.bias",
+        module_name="NeuralNet"
+    )
+    torcheck.add_tensor_changing_check(
+        changing_model.fc2.weight,
+        tensor_name="fc2.weight",
+        module_name="NeuralNet"
+    )
+    torcheck.add_tensor_changing_check(
+        changing_model.fc2.bias,
+        tensor_name="fc2.bias",
+        module_name="NeuralNet"
+    )
+    run_training(changing_model, dataloader, changing_model_optimizer)
+
+
+def test_tensor_changing_check_with_unchanging_model(
+    unchanging_model_optimizer,
+    unchanging_model,
+    dataloader,
+    run_training
+):
+    torcheck.register(unchanging_model_optimizer)
+    torcheck.add_tensor_changing_check(
+        unchanging_model.fc1.weight,
+        tensor_name="fc1.weight",
+        module_name="NeuralNet"
+    )
+    torcheck.add_tensor_changing_check(
+        unchanging_model.fc1.bias,
+        tensor_name="fc1.bias",
+        module_name="NeuralNet"
+    )
+    torcheck.add_tensor_changing_check(
+        unchanging_model.fc2.weight,
+        tensor_name="fc2.weight",
+        module_name="NeuralNet"
+    )
+    torcheck.add_tensor_changing_check(
+        unchanging_model.fc2.bias,
+        tensor_name="fc2.bias",
+        module_name="NeuralNet"
+    )
+    with pytest.raises(
+        RuntimeError,
+        match=r"Module NeuralNet's fc1\.weight should change\.\n.*fc1.bias should change"
+    ):
+        run_training(unchanging_model, dataloader, unchanging_model_optimizer)
+
+
+def test_tensor_unchanging_check_with_changing_model(
+    changing_model_optimizer,
+    changing_model,
+    dataloader,
+    run_training
+):
+    torcheck.register(changing_model_optimizer)
+    torcheck.add_tensor_unchanging_check(
+        changing_model.fc1.weight,
+        tensor_name="fc1.weight",
+        module_name="NeuralNet"
+    )
+    torcheck.add_tensor_unchanging_check(
+        changing_model.fc1.bias,
+        tensor_name="fc1.bias",
+        module_name="NeuralNet"
+    )
+    with pytest.raises(
+        RuntimeError,
+        match=r"Module NeuralNet's fc1\.weight should not change\.\n.*fc1.bias should not change"
+    ):
+        run_training(changing_model, dataloader, changing_model_optimizer)
+
+
+def test_tensor_unchanging_check_with_unchanging_model(
+    unchanging_model_optimizer,
+    unchanging_model,
+    dataloader,
+    run_training
+):
+    torcheck.register(unchanging_model_optimizer)
+    torcheck.add_tensor_unchanging_check(
+        unchanging_model.fc1.weight,
+        tensor_name="fc1.weight",
+        module_name="NeuralNet"
+    )
+    torcheck.add_tensor_unchanging_check(
+        unchanging_model.fc1.bias,
+        tensor_name="fc1.bias",
+        module_name="NeuralNet"
+    )
+    run_training(unchanging_model, dataloader, unchanging_model_optimizer)
+
